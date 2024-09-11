ce8b5525da35a393d3fde27222de9618c56e4e78
Daniel-Esteban-Saenz <daniel_Saenz@soy.sena.edu.co>
2024-09-11 16:44:14 -0500

    agrege un archivo

--- prueva.py
+++ prueva.py
@@ -1,6 +1,11 @@
 import time
 
-print("Hello world");
+print("hola") #esta linea imprime en la consola la palabra hola
+print("Hello world \n");
 print("segundo renglòn");
+
 time.sleep(3);
-print("hola");
+
+print(type("hola")) #la funcion type me dice en la consola los tipos de datos que son
+print(type(4))
+print(type(5.5))
