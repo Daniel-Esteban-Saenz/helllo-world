84012a937f92334f5657f713c798ddd47ff45fbb
Daniel1017 <daniel_saenz@soy.sena.edu.co>
2024-09-16 14:46:18 -0500

    prueva archivogitignore2

--- prueva.py
+++ prueva.py
@@ -1,5 +1,4 @@
 import time
-
 print("hola") #esta linea imprime en la consola la palabra hola
 print("Hello world \n");
 print("segundo renglòn");
@@ -9,3 +8,4 @@ time.sleep(3);
 print(type("hola")) #la funcion type me dice en la consola los tipos de datos que son
 print(type(4))
 print(type(5.5))
+print("pruevagitignore");
